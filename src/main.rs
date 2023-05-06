8b73322fb97643ed8e592f96a0b71909ac2018bd
Arash Bannazadeh-Mahani <axbannaz@gmail.com>
2023-05-05 18:21:27 -0700

    remove tracing

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 use once_cell::sync::Lazy;
 use opentelemetry_api::global;
-use opentelemetry_api::global::shutdown_tracer_provider;
-use opentelemetry_api::trace::TraceError;
+// use opentelemetry_api::global::shutdown_tracer_provider;
+// use opentelemetry_api::trace::TraceError;
 use opentelemetry_api::{
     metrics,
-    trace::{TraceContextExt, Tracer},
-    Context, Key, KeyValue,
+    // trace::{TraceContextExt, Tracer},
+    Context, KeyValue,
 };
 use opentelemetry_otlp::{ExportConfig, WithExportConfig};
-use opentelemetry_sdk::{metrics::MeterProvider, runtime, trace as sdktrace, Resource};
+use opentelemetry_sdk::{metrics::MeterProvider, runtime}; //, Resource};
 use std::error::Error;
+use std::time::Duration;
 
-fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
-    opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(
-            opentelemetry_otlp::new_exporter()
-                .tonic()
-                .with_endpoint("http://localhost:4317"),
-        )
-        .with_trace_config(
-            sdktrace::config().with_resource(Resource::new(vec![KeyValue::new(
-                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-                "trace-demo",
-            )])),
-        )
-        .install_batch(runtime::Tokio)
-}
+// fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
+//     opentelemetry_otlp::new_pipeline()
+//         .tracing()
+//         .with_exporter(
+//             opentelemetry_otlp::new_exporter()
+//                 .tonic()
+//                 .with_endpoint("http://localhost:4317"),
+//         )
+//         .with_trace_config(
+//             sdktrace::config().with_resource(Resource::new(vec![KeyValue::new(
+//                 opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+//                 "trace-demo",
+//             )])),
+//         )
+//         .install_batch(runtime::Tokio)
+// }
 
 fn init_metrics() -> metrics::Result<MeterProvider> {
     let export_config = ExportConfig {
@@ -40,11 +41,14 @@ fn init_metrics() -> metrics::Result<MeterProvider> {
                 .tonic()
                 .with_export_config(export_config),
         )
+        .with_period(Duration::from_secs(1))
+        // .with_aggregation_selector(selector)
+        // .with_resource(resource)
         .build()
 }
 
-const LEMONS_KEY: Key = Key::from_static_str("lemons");
-const ANOTHER_KEY: Key = Key::from_static_str("ex.com/another");
+// const LEMONS_KEY: Key = Key::from_static_str("lemons");
+// const ANOTHER_KEY: Key = Key::from_static_str("ex.com/another");
 
 static COMMON_ATTRIBUTES: Lazy<[KeyValue; 1]> = Lazy::new(|| {
     [
@@ -60,11 +64,11 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // By binding the result to an unused variable, the lifetime of the variable
     // matches the containing block, reporting traces and metrics during the whole
     // execution.
-    let _ = init_tracer()?;
+    // let _ = init_tracer()?;
     let meter_provider = init_metrics()?;
     let cx = Context::new();
 
-    let tracer = global::tracer("ex.com/basic");
+    // let tracer = global::tracer("ex.com/basic");
     let meter = global::meter("ex.com/basic");
 
     let gauge = meter
@@ -78,10 +82,12 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         .init();
 
     meter.register_callback(&[gauge.as_any()], move |observer| {
+        println!("{:?}", gauge);
         observer.observe_f64(&gauge, std::f64::consts::E, COMMON_ATTRIBUTES.as_ref());
     })?;
 
     meter.register_callback(&[counter.as_any()], move |observer| {
+        println!("{:?}", counter);
         observer.observe_f64(&counter, std::f64::consts::PI, COMMON_ATTRIBUTES.as_ref());
     })?;
 
@@ -91,25 +97,26 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         .init();
     histogram.record(&cx, 5.5, COMMON_ATTRIBUTES.as_ref());
 
-    tracer.in_span("operation", |cx| {
-        let span = cx.span();
-        span.add_event(
-            "Nice operation!".to_string(),
-            vec![Key::new("bogons").i64(100)],
-        );
-        span.set_attribute(ANOTHER_KEY.string("yes"));
+    // tracer.in_span("operation", |cx| {
+    //     let span = cx.span();
+    //     span.add_event(
+    //         "Nice operation!".to_string(),
+    //         vec![Key::new("bogons").i64(100)],
+    //     );
+    //     span.set_attribute(ANOTHER_KEY.string("yes"));
 
-        tracer.in_span("Sub operation...", |cx| {
-            let span = cx.span();
-            span.set_attribute(LEMONS_KEY.string("five"));
+    //     tracer.in_span("Sub operation...", |cx| {
+    //         let span = cx.span();
+    //         span.set_attribute(LEMONS_KEY.string("five"));
 
-            span.add_event("Sub span event", vec![]);
+    //         span.add_event("Sub span event", vec![]);
 
-            histogram.record(&cx, 1.3, &[]);
-        });
-    });
+    //         histogram.record(&cx, 1.3, &[]);
+    //     });
+    // });
 
-    shutdown_tracer_provider();
+    std::thread::sleep(Duration::from_secs(60));
+    // shutdown_tracer_provider();
     meter_provider.force_flush(&cx)?;
     meter_provider.shutdown()?;
 
